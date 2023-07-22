c20f07e85c61de9d828b8964fd940356ed154fcf
JaN0h4ck <95576868+JaN0h4ck@users.noreply.github.com>
2023-07-22 15:31:00 +0200

    Ue1A2

--- Uebung1/TestPlain2D.cpp
+++ Uebung1/TestPlain2D.cpp
@@ -14,8 +14,25 @@ void test() {
     }
 }
 
+void drawSquare() {
+    ViewPortGL vp = ViewPortGL("OpenGL Square", 1000, 1000);
+    bool isRunning = true;
+    while (isRunning) {
+        isRunning = !vp.windowShouldClose();
+
+        vp.prepareLine(299, 299, 699, 299, 255, 255, 255);
+        vp.prepareLine(299, 299, 299, 699, 255, 255, 255);
+        vp.prepareLine(699, 299, 699, 699, 255, 255, 255);
+        vp.prepareLine(299, 699, 699, 699, 255, 255, 255);
+        vp.sendLines();
+
+        vp.swapBuffers();
+    }
+}
+
 int main() {
-    test();
+    //test();
+    drawSquare();
     return 0;
 }
 
